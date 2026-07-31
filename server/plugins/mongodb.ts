54c6b6b8eebbf39e63bab8391e198a010a6b1c9e
Eric Roy Anderson <eric@booksuey.com>
2026-07-31 10:52:40 +0200

    db connection cache fix

--- server/plugins/mongodb.ts
+++ server/plugins/mongodb.ts
@@ -1,14 +1,45 @@
 import type { Nitro } from "nitropack";
 import mongoose from "mongoose";
 
+const MONGODB_URI = process.env.MONGO_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGO_URI environment variable");
+}
+
+let cached = global.mongoose || { conn: null, promise: null };
+
+if (!global.mongoose) {
+  global.mongoose = cached;
+}
+
 export default async (_nitroApp: Nitro) => {
-  //run your connect code here
   const config = useRuntimeConfig();
-  //connect to mongodb
+  
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+      maxPoolSize: 10,
+      minPoolSize: 2,
+    };
+
+    cached.promise = mongoose.connect(config.MONGO_URI || MONGODB_URI, opts).then((mongoose) => {
+      return mongoose;
+    });
+  }
+
   try {
-    await mongoose.connect(config.MONGO_URI);
+    cached.conn = await cached.promise;
     console.log("Connected to MongoDB");
   } catch (e) {
-    console.error(e);
+    cached.promise = null;
+    console.error("Failed to connect to MongoDB:", e);
+    throw e;
   }
+
+  return cached.conn;
 };
